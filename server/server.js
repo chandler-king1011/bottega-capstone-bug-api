70da957d004c2b2e9608f74ed4e4eae87f91bf93
Chandler King <ckmotox356@gmail.com>
2020-01-04 12:16:42 -0700

    Added CORS ability to the API

--- server/server.js
+++ server/server.js
@@ -16,6 +16,12 @@ const port = process.env.PORT || 3000;
 app.use(bodyparser.json());
 app.use(cookieParser());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+  });
+
 
 
 
