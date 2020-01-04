080837a151e03d9ba32755f7abb3efa1ebfa15e1
Chandler King <ckmotox356@gmail.com>
2020-01-04 12:52:28 -0700

    Fixed CORS issues using the cors middleware.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 
 
@@ -15,12 +16,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(bodyparser.json());
 app.use(cookieParser());
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   next();
-  });
+app.use(cors());
 
 
 
