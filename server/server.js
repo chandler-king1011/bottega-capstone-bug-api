c03ce580366b011d8d346f4013c78f627fdc78d8
Chandler King <ckmotox356@gmail.com>
2020-01-06 16:50:08 -0700

    Switched back over to using the cors library to avoid cors errors on the front end.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const cors = require("cors");
 
 
 
@@ -14,22 +15,12 @@ const organizationRouter = require('./routes/organization');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 app.use(bodyparser.json());
 app.use(cookieParser());
 
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    if (req.method === "OPTIONS") {
-        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-        return res.status(200).json({});
-    }
-    next();
-});
 
 
 
