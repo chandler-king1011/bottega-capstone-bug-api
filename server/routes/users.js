e8bc9b9c5517de915d02b0d6e62415dbb5bc9896
Chandler King <ckmotox356@gmail.com>
2020-02-14 16:32:44 -0700

    I created three new routes for the reset password feature. One route for sending an email to the user with the reset password link and creating a token, another for verifying the token when the component mounts on the frontend, and the final route for updating the password.

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,11 @@
 const mysql = require('mysql');
 const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcrypt');
+const crypto = require('crypto');
 const jwt = require("jsonwebtoken");
 const verify = require("./verifyToken");
+const sgMail = require('@sendgrid/mail');
+const moment = require('moment');
 
 
 require('dotenv').config();
@@ -105,6 +108,113 @@ function usersRouter(app) {
             })
         }
     });
+
+    //reset password request
+
+    app.post("/users/reset-password/request", (req, res) => {
+        const email = req.body.email;
+        dbConn.query("SELECT users_id, users_first_name, users_last_name ,users_email FROM users WHERE users_email = ?", 
+        [email],
+        (error, results) => {
+            if (error) {
+                res.send({"status": 400, "message": "No such email please try again.", "errors": error})
+            } else {
+                if (results.length > 0) {
+                    let token = crypto.randomBytes(20).toString('hex');
+                    let updateInfo = {
+                        reset_password_token: token,
+                        reset_token_exp: moment().add(1, 'hours').format()
+                    }
+                    dbConn.query("UPDATE users SET ? WHERE users_id = ?",
+                    [updateInfo, results[0].users_id],
+                    (err, response) => {
+                        if (err) {
+                            res.send({"status": 400, "message": "An error occurred please try again.", "errors": err})
+                        } else {
+                            sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+                            const msg = {
+                                to: results[0].users_email,
+                                from: 'resetpassword@test.com',
+                                subject: 'Bug Eliminator Password Reset Request',
+                                text: 
+                                    'You are receiving this email because someone has requested to change the password for your account.\n\n'
+                                  + 'Please use the following link to reset your password. This link will only be good for one hour \n\n'
+                                  + `http://localhost:3000/password/reset/${token}\n\n`
+                                  + 'If you did not request this email, please ignore this message and your password will remain the unchanged.'
+                            };
+                            sgMail.send(msg);
+                            res.status(200).send({"message": "An email has been sent to your email. Please refer to this email for further instruction."});
+                        }
+                    })
+                } else if (results.length === 0) {
+                    res.send({"status": 400, "message": "No such email please try again."})
+                }
+            }
+        })
+    });
+
+
+
+    //reset password token verification for component mounting purposes
+
+    app.post("/users/reset-password/token-verification", (req, res) => {
+        const token = req.body.token;
+        dbConn.query("SELECT reset_token_exp FROM users WHERE reset_password_token = ?", 
+        [token], 
+        (error, results) => {
+            if (error) {
+                res.status(400).send({"message": "An error occurred verifying your token. Please try another reset password request."})
+            } else {
+                let now = moment().format();
+                if (moment(now).isBefore(results[0].reset_token_exp)) {
+                    res.status(200).send({"message": "Your token is still valid."});
+                } else {
+                    res.status(400).send({"message": "Your token has expired"});
+                }
+            }
+        })
+    })
+
+    //reset password
+
+    app.put("/users/reset-password/update", 
+    [
+        check('newPassword', 'Password must be between 8-100 characters long').isLength({min: 8, max: 100}),
+        check('newPassword', 'Password must contain one lowercase, one uppercase, a number, and a special character.')
+        .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.* )(?=.*[^a-zA-Z0-9]).{8,}$/, "i")
+    ],
+    (req, res) => {
+        const validationErrors = validationResult(req);
+        const newPassword = req.body.newPassword;
+        const token = req.body.token;
+        if (!validationErrors.isEmpty()) {
+            res.status(400).send({"message": "Invalid input. Make sure your new password meets all of the required criteria.", "errors": validationErrors});
+        } else {
+            bcrypt.hash(newPassword, saltRounds, (err, hash) => {
+                if(err) {
+                    res.status(400).send({"message": "A problem occurred while hashing your password. Please try again.", "errors": err});
+                } else {
+                    const userUpdateData = {
+                        users_password: hash,
+                        reset_password_token: null,
+                        reset_token_exp: null
+                    }
+                    dbConn.query("UPDATE users SET ? WHERE  reset_password_token = ?", 
+                    [userUpdateData, token],
+                    (error, results) => {
+                        if (error) {
+                            res.status(400).send({"message": "Could not update your password. Please try again.", "errors": error});
+                        } else {
+                            res.status(200).send({"message": "Your password has been updated successfully. Return to the login page to login."})
+                        }
+                    })
+                }
+            })
+        }
+
+    }) 
+
+    
 
 
 
